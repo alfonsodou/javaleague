f736810ef2251cc37fe079fe1b0c85ad1da53ba5
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2015-03-26 11:50:39 +0100

    First commit

---
+++ target/.generated/org/javahispano/javaleague/client/Messages_.java
@@ -0,0 +1,12 @@
+package org.javahispano.javaleague.client;
+
+public class Messages_ implements org.javahispano.javaleague.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
