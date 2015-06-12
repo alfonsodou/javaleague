67af636f15a1a0cb245425dd93c3e6d876218874
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2015-06-12 14:29:38 +0200

    Añadidos logos

--- src/main/java/org/javahispano/javaleague/client/application/ApplicationPresenter.java
+++ src/main/java/org/javahispano/javaleague/client/application/ApplicationPresenter.java
@@ -12,8 +12,8 @@ import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window;
 import com.google.web.bindery.event.shared.EventBus;
-import com.gwtplatform.mvp.client.HasUiHandlers;
 import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.Presenter.RevealType;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.ContentSlot;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
@@ -23,9 +23,9 @@ import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
 import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 
 public class ApplicationPresenter extends
-		Presenter<ApplicationPresenter.MyView, ApplicationPresenter.MyProxy>
-		implements ApplicationUiHandlers {
-	interface MyView extends View, HasUiHandlers<ApplicationUiHandlers> {
+		Presenter<ApplicationPresenter.MyView, ApplicationPresenter.MyProxy> {
+		
+	interface MyView extends View {
 	}
 
 	@ContentSlot
@@ -33,19 +33,13 @@ public class ApplicationPresenter extends
 	// new Type<>();
 	public static final GwtEvent.Type<RevealContentHandler<?>> SLOT_SetMainContent = new GwtEvent.Type<RevealContentHandler<?>>();
 
-	private final PlaceManager placeManager;
-
 	@ProxyStandard
 	interface MyProxy extends Proxy<ApplicationPresenter> {
 	}
 
 	@Inject
-	ApplicationPresenter(EventBus eventBus, MyView view, MyProxy proxy,
-			PlaceManager placeManager) {
+	ApplicationPresenter(EventBus eventBus, MyView view, MyProxy proxy) {
 		super(eventBus, view, proxy, RevealType.Root);
-		getView().setUiHandlers(this);
-
-		this.placeManager = placeManager;
 
 		// Making the window scroll to top on every page change
 		History.addValueChangeHandler(new ValueChangeHandler<String>() {
@@ -59,36 +53,6 @@ public class ApplicationPresenter extends
 				});
 			}
 		});
-	}
-
-	private void goTo(String nameToken) {
-		placeManager.revealPlace(new PlaceRequest.Builder()
-				.nameToken(nameToken).build());
-	}
-
-	@Override
-	public void doShowHomePage() {
-		goTo(NameTokens.getHome());
-	}
-
-	@Override
-	public void doShowLoginPage() {
-		goTo(NameTokens.getLogin());
-	}
-
-	@Override
-	public void doShowRulesPage() {
-		goTo(NameTokens.getRules());
-	}
-
-	@Override
-	public void doShowDownloadPage() {
-		goTo(NameTokens.getDownloads());
-	}
-
-	@Override
-	public void doShowTournamentPage() {
-		goTo(NameTokens.getTournament());
 	}
 
 }
--- src/main/java/org/javahispano/javaleague/client/application/ApplicationView.java
+++ src/main/java/org/javahispano/javaleague/client/application/ApplicationView.java
@@ -2,16 +2,14 @@ package org.javahispano.javaleague.client.application;
 
 import javax.inject.Inject;
 
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import com.gwtplatform.mvp.client.ViewImpl;
 
-class ApplicationView extends ViewWithUiHandlers<ApplicationUiHandlers>
+class ApplicationView extends ViewImpl
 		implements ApplicationPresenter.MyView {
 
 	interface Binder extends UiBinder<Widget, ApplicationView> {
@@ -32,29 +30,5 @@ class ApplicationView extends ViewWithUiHandlers<ApplicationUiHandlers>
 		} else {
 			super.setInSlot(slot, content);
 		}
-	}
-
-	@UiHandler("loginButton")
-	public void onLoginClick(ClickEvent event) {
-		if (getUiHandlers() != null)
-			getUiHandlers().doShowLoginPage();
-	}
-
-	@UiHandler("downloadButton")
-	public void onDownloadClick(ClickEvent event) {
-		if (getUiHandlers() != null)
-			getUiHandlers().doShowDownloadPage();
-	}
-
-	@UiHandler("rulesButton")
-	public void onRulesClick(ClickEvent event) {
-		if (getUiHandlers() != null)
-			getUiHandlers().doShowRulesPage();
-	}
-
-	@UiHandler("tournamentButton")
-	public void onTournamentClick(ClickEvent event) {
-		if (getUiHandlers() != null)
-			getUiHandlers().doShowTournamentPage();
 	}
 }
