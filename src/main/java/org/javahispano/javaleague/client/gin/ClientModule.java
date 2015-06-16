53e4507501ff4212a03685bacd9bb8c4ccf191a5
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2015-06-16 15:57:37 +0200

    Creada estructura para RPC-Dispatch

--- src/main/java/org/javahispano/javaleague/client/gin/ClientModule.java
+++ src/main/java/org/javahispano/javaleague/client/gin/ClientModule.java
@@ -2,6 +2,8 @@ package org.javahispano.javaleague.client.gin;
 
 import org.javahispano.javaleague.client.application.ApplicationModule;
 import org.javahispano.javaleague.client.place.NameTokens;
+
+import com.gwtplatform.dispatch.rpc.client.gin.RpcDispatchAsyncModule;
 import com.gwtplatform.mvp.client.annotations.DefaultPlace;
 import com.gwtplatform.mvp.client.annotations.ErrorPlace;
 import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
@@ -9,6 +11,7 @@ import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 import com.gwtplatform.mvp.client.gin.DefaultModule;
 import com.gwtplatform.mvp.shared.proxy.RouteTokenFormatter;
 import com.arcbees.analytics.client.AnalyticsModule;
+
 import org.javahispano.javaleague.client.place.NavigationTracker;
 
 /**
@@ -19,6 +22,7 @@ public class ClientModule extends AbstractPresenterModule {
     @Override
     protected void configure() {
         install(new DefaultModule.Builder().tokenFormatter(RouteTokenFormatter.class).build());
+        install(new RpcDispatchAsyncModule());
         install(new ApplicationModule());
 
         // DefaultPlaceManager Places
